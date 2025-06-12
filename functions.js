b5052b063647662383c92b95eb6cb9f3f3dd6180
anil0234 <banothanil388@gmail.com>
2025-06-12 06:55:44 +0000

    functions

---
+++ functions.js
@@ -0,0 +1,44 @@
+// function are one of the most powerful features in java scripts
+//
+//
+// They alloe you to reuse the powerful features
+
+// Functions are used always like declaration + calls
+//syntax
+// Function declaration
+sayHello()
+function sayHello(){
+    console.log("Hello")
+} 
+
+
+sayHello()
+// // Function calling 
+// sayHello()
+// sayHello()
+ 
+
+// console.log(a)
+// let a = 10;
+
+function sum(num1 , num2) {
+    console.log(num1 + num2)
+}
+sum(10,5) 
+
+ // Functions with Parameters
+
+ function sum(num1,num2,num3){
+    console.log(num1 + num2 + num3)
+ }
+ sum(10,10,10)
+
+ function mul(a,b,c){ 
+    console.log(a*b*c)
+ }
+ mul(5,10,5)
+
+function div(a,b){
+    console.log(a/b)
+}
+div(10,20)
